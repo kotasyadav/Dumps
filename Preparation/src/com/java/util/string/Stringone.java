3f38154827f3c6b1504f14a840d934a2101351b6
kotasyadav <kotasyadav@gmail.com>
2017-09-06 22:51:47 +0530

    commit

---
+++ Preparation/src/com/java/util/string/Stringone.java
@@ -0,0 +1,12 @@
+package com.java.util.string;
+
+public class Stringone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hai");
+
+	}
+
+}
